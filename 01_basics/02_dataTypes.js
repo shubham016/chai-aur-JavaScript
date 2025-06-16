7a36bb7542c722b9713847426d27c64f859cd403
Shubham <43635426+shubham016@users.noreply.github.com>
2025-06-16 10:11:43 +0000

    Primitive Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let name = "shubham" //string
+let age  = 25 //Number
+let student = true //boolean types
+let state; //undefined state
+let temperature = null // also can be called 0 // and also and object
+
+//permitive datatypes
+
+//string
+//number 
+//boolean
+//null => stand alone value => empty value => 0 => null also known as object
+//undefined = > mentioned above
+
+//object 
+console.log(typeof temperature);
