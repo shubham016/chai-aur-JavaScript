321775284c3777180adee037e2a37a88c9a84166
Shubham <43635426+shubham016@users.noreply.github.com>
2025-06-16 09:27:35 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "web.shubham1@gmail..com"
+var accountName = "shubham"
+accountCity = "kathmandu"
+let accountType;
+
+
+// accountId = 1
+accountEmail = "web.hero@gmail.com"
+accountName = "Rajbanshi"
+accountCity = "Lalitpur"
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountName, accountCity, accountType])
