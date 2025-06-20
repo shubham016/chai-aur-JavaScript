60edfc31916e1452bc2c54efe600c2f4743463a1
Shubham <43635426+shubham016@users.noreply.github.com>
2025-06-20 08:09:31 +0000

    various datatypes

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //instructs js file to use it in newer version
+
+// alert(2 + 2); //gets displayed in browser popup, but in this file we are using the node.js environment file so won't display here
+
+
+let name = "shubham" //string datatype
+var number = 7 //number datatype
+const statement = true | false; //boolean datatype
+
+//primitive datatypes
+/*
+number = 9
+string = "text"
+boolean = true/false
+null = standalone value(0) also considered as "0".
+undefined = //empty value after any variable(let, var, const) is decleared.
+symbol = unique (eg: emoji & other symbols)
+*/
+
+console.log(typeof name);  //defines which type of datatype variable has used.
+console.log(typeof Number);  //defines number datatype.
+console.log(typeof statement);  //defines number datatype.
+console.log(typeof null);  //defines "object" datatype.
+console.log(typeof undefined);  //defines "undefined" datatype.
